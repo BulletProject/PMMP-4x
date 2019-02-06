de5230c176034a6f00bd7c55903f13cd61873b34
haniokasai <htek@haniokasai.com>
2019-02-06 22:14:40 +0900

    no message

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -37,7 +37,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 
 	const NAME = "PocketMine-MP";
-	const BASE_VERSION = "3.5.9";
+	const BASE_VERSION = "3.5.10";
 	const IS_DEVELOPMENT_BUILD = false;
 	const BUILD_NUMBER = 1;
 
